b0cecd10ffb6bf161c349de4273c3e84cd320e85
nhi-ngo <nhingo.y@gmail.com>
2021-02-17 10:27:02 -0500

    install dotenv and deploy

--- server/index.js
+++ server/index.js
@@ -2,24 +2,27 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
-
-const CONNECTION_URL =
-  'mongodb+srv://nhingo01:Doyourbest01@cluster0.ikprc.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.get('/', (req, res) => res.send('Hello to Notes API'));
 
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((err) => console.log(`${err} did not connect`));
 
