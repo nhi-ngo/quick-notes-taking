2051e33f8fab4b8a3f5d26131220dfdb303a4a7b
nhi-ngo <nhingo.y@gmail.com>
2022-08-22 22:35:15 -0400

    Added Navbar, Auth folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import { useDispatch } from 'react-redux';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { getPosts } from './actions/posts';
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
-import notes from './images/notes.png';
-import useStyles from './styles';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
 
-export default function App() {
-  const [currentId, setCurrentId] = useState(0);
-  const dispatch = useDispatch();
-  const classes = useStyles();
-
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId]);
-
-  return (
+const App = () => (
+  <BrowserRouter>
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">
-          Quick Notes
-        </Typography>
-        <img className={classes.image} src={notes} alt="notes" height="60" />
-      </AppBar>
-
-      <Grow in>
-        <Container>
-          <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} md={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-
-            <Grid item xs={12} md={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+      </Switch>
     </Container>
-  );
-}
+  </BrowserRouter>
+);
+
+export default App;
