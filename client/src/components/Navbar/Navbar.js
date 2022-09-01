32c2ed001df69bac90abbee1fc9b675ff5e11bf9
nhi-ngo <nhingo.y@gmail.com>
2022-08-31 20:31:53 -0400

    add sign in functionality

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from 'react';
+import {
+  Container, AppBar, Typography, Toolbar, Avatar, Button,
+} from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import notes from '../../images/notes.png';
+import useStyles from './styles';
+import { LOGOUT } from '../../constants/actionTypes';
+import * as actionType from '../../constants/actionTypes';
+
+const Navbar = () => {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const history = useHistory();
+  const classes = useStyles();
+
+  const [user, setUser] = useState();
+
+  const logout = () => {
+    dispatch({ type: actionType.LOGOUT });
+    history.push('/signin');
+    setUser(null);
+  };
+
+  useEffect(() => {
+    // setUser(JSON.parse(localStorage.getItem('profile')));
+    // setUser();
+  }, []);
+
+  return (
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <div className={classes.brandContainer}>
+          <Typography component={Link} to="/" className={classes.heading} variant="h2" align="center">
+            Quick Notes
+          </Typography>
+
+          <img className={classes.image} src={notes} alt="notes" height="60" />
+        </div>
+
+        <Toolbar className={classes.toolbar}>
+          {user?.result ? (
+            <div className={classes.profile}>
+              <Avatar className={classes.purple} alt={user?.result.name} src={user?.result.imageUrl}>
+                {user?.result.name.charAt(0)}
+              </Avatar>
+              <Typography className={classes.userName} variant="h6">
+                {user?.result.name}
+              </Typography>
+              <Button variant="contained" className={classes.logout} color="secondary" onClick={logout}>
+                Logout
+              </Button>
+            </div>
+          ) : (
+            <Button component={Link} to="/signin" variant="contained" color="primary">
+              Sign In
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ client/src/components/Navbar/styles.js
@@ -0,0 +1,54 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles((theme) => ({
+	appBar: {
+		borderRadius: 15,
+		margin: '30px 0',
+		display: 'flex',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		padding: '10px 50px',
+	},
+	heading: {
+		color: '#F7C652',
+		fontFamily: 'Hachi Maru Pop',
+		marginTop: '-15px',
+		textDecoration: 'none',
+
+		[theme.breakpoints.down('md')]: {
+			fontSize: '3rem',
+		},
+
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '2.5rem',
+		},
+	},
+	image: {
+		marginLeft: '15px',
+	},
+	toolbar: {
+		display: 'flex',
+		justifyContent: 'flex-end',
+		width: '400px',
+	},
+	profile: {
+		display: 'flex',
+		justifyContent: 'flex-end',
+	},
+	userName: {
+		display: 'flex',
+		alignItems: 'center',
+		marginLeft: '10px',
+		marginRight: '10px',
+	},
+	brandContainer: {
+		display: 'flex',
+		alignItems: 'center',
+	},
+	[theme.breakpoints.down('sm')]: {
+		mainContainer: {
+			flexDirection: 'column-reverse',
+		},
+	},
+}));
