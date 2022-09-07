eb511d584d44521f19c722900f219519099fe05e
nhi-ngo <nhingo.y@gmail.com>
2022-09-07 19:53:24 -0400

    remove loading indicator

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import { Grid, CircularProgress } from '@material-ui/core';
+import { Grid } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 import Post from './Post/Post';
 
 import useStyles from './styles';
 
 export default function Posts({ setCurrentId }) {
-  const posts = useSelector((state) => state.posts);
+	const posts = useSelector((state) => state.posts);
 
-  const classes = useStyles();
+	const classes = useStyles();
 
-  return (
-    !posts.length ? <CircularProgress /> : (
-      <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3}>
-        {posts.map((post) => (
-          <Grid key={post._id} item xs={12} sm={6}>
-            <Post post={post} setCurrentId={setCurrentId} />
-          </Grid>
-        ))}
-      </Grid>
-    )
-
-  );
+	return (
+		<Grid className={classes.mainContainer} container alignItems='stretch' spacing={3}>
+			{posts.map((post) => (
+				<Grid key={post._id} item xs={12} sm={6}>
+					<Post post={post} setCurrentId={setCurrentId} />
+				</Grid>
+			))}
+		</Grid>
+	);
 }
