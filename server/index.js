b63c66050c3e898851978cdecc3d28f90876cc11
nhi-ngo <nhingo.y@gmail.com>
2026-05-04 20:00:18 -0500

    upgrade dependencies for server

--- server/index.js
+++ server/index.js
@@ -10,35 +10,27 @@ import authRoutes from './routes/auth.js';
 const app = express();
 dotenv.config();
 
+mongoose.set('strictQuery', true);
+
 const connect = () => {
 	mongoose
-		.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true })
-		.then(() => {
-			console.log('Connected to DB');
-		})
-		.catch((err) => {
-			throw err;
-		});
+		.connect(process.env.MONGO_URI)
+		.then(() => console.log('Connected to DB'))
+		.catch((err) => console.log(err));
 };
 
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
 app.use('/api/posts', postRoutes);
 app.use('/api/auth', authRoutes);
+
 app.get('/', (req, res) => res.send('Hello to Notes API'));
 
-app.use((err, req, res, next) => {
-	const status = err.status || 500;
-	const message = err.message || 'Something went wrong!';
-	return res.status(status).json({
-		success: false,
-		status,
-		message,
-	});
-});
+const PORT = process.env.PORT || 5001;
 
-app.listen(process.env.PORT || 5000, () => {
+app.listen(PORT, () => {
 	connect();
-	console.log('Connected to Server');
+	console.log(`Server running on port ${PORT}`);
 });
