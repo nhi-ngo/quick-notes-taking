18a38fe2e41475cefcc3d479d18b3b0ebdd164cf
nhi-ngo <nhingo.y@gmail.com>
2026-05-08 18:45:18 -0500

    allow cors backend from vercel

--- server/index.js
+++ server/index.js
@@ -12,7 +12,11 @@ dotenv.config();
 
 mongoose.set('strictQuery', true);
 
-app.use(cors());
+app.use(
+	cors({
+		origin: '*',
+	}),
+);
 app.use(cookieParser());
 app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
